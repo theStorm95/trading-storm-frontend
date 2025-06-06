41a81402f41802ddf5f4b1da0b2942958229e49a
Nathan Storm <nathanstorm95@gmail.com>
2025-06-06 11:47:37 -0700

    test: added vitest globals

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -1,5 +1,3 @@
-import { expect, it, describe } from "vitest";
-
 describe("App component", () => {
   it("renders Hello world", () => {
     expect(true).toBe(true);
--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,6 @@ export default defineConfig({
     coverage: {
       reporter: ["text", "json", "html"],
     },
+    globals: true,
   },
 });
